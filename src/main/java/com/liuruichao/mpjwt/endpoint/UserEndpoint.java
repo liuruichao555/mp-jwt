a63b853f5337b187d7e87e83b004d542acf9bf1d
liuruichao <liuruichao555@126.com>
2018-06-06 17:05:20 +0800

    del test code

--- src/main/java/com/liuruichao/mpjwt/endpoint/UserEndpoint.java
+++ src/main/java/com/liuruichao/mpjwt/endpoint/UserEndpoint.java
@@ -10,10 +10,12 @@ import javax.annotation.security.DenyAll;
 import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import java.util.Set;
 
 /**
  * UserEndpoint
@@ -24,11 +26,12 @@ import javax.ws.rs.QueryParam;
 @ApplicationScoped
 @DeclareRoles({"ViewBalance", "Debtor", "Creditor", "Debtor2", "BigSpender"})
 @Path("/user")
+@Produces(MediaType.APPLICATION_JSON)
 public class UserEndpoint {
     @Inject
     private JsonWebToken jsonWebToken;
 
-    @GET
+    /*@GET
     public String login(@QueryParam("username") String username, @QueryParam("password") String password) throws Exception {
         if (username.equals("liuruichao") && password.equals("liuruichao123")) {
 
@@ -37,7 +40,7 @@ public class UserEndpoint {
             return "success";
         }
         return "fail";
-    }
+    }*/
 
     @POST
     @RolesAllowed({"ViewBalance"})
@@ -45,4 +48,32 @@ public class UserEndpoint {
         System.out.println(jsonWebToken.getRawToken());
         return "100";
     }
+
+    @Path("/name")
+    @GET
+    @RolesAllowed({"Creditor"})
+    public Response getGroupd() {
+        return Response.ok(jsonWebToken.getGroups()).build();
+    }
+
+    /*@GET
+    @Path("/debit")
+    @RolesAllowed({"Debtor", "Debtor2"})
+    public String debit(@QueryParam("amount") String amount,
+                          @Context SecurityContext securityContext) {
+        Double damount = Double.valueOf(amount);
+        if(damount > bigSpenderLimit) {
+            if(securityContext.isUserInRole("BigSpender")) {
+                // Validate the spending limit from the token claim
+                JsonNumber spendingLimit = jwt.getClaim("spendingLimit");
+                if(spendingLimit == null || spendingLimit.doubleValue() < damount) {
+                    return Response.status(Response.Status.BAD_REQUEST).build();
+                }
+            } else {
+                return Response.status(Response.Status.FORBIDDEN).build();
+            }
+        }
+        usdBalance = usdBalance.subtract(new BigDecimal(amount));
+        return Response.ok(generateBalanceInfo()).build();
+    }*/
 }
